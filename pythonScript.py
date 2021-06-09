8333e85fda977ab6f257878b6e62255101de46ea
Merlingot <merlingot1@gmail.com>
2021-06-09 18:00:51 -0400

    fonctionnel

--- pythonScript.py
+++ pythonScript.py
@@ -1,10 +1,17 @@
 import cv2
 import numpy as np
 
-def foo_bar(img=None): 
+def foo_bar(img=None):
+    # On reçois une image de la partie c++ 
     if img is not None:
-        print('image received')
-        return img
+        # Si image reçue, on l'enregistre dans le répertoire (c'est juste pour tester)
+        cv2.imwrite('../image_recue.png', img)
+        # Faire quelque chose avec l'image ici ...
+        
+        # Je retourne un array à la partie c++
+        direction = np.array([1,2,3], dtype=np.uint8) 
+        
+        return direction
     else:
-        print('no image')
+        # Pas d'image reçue
         return None
